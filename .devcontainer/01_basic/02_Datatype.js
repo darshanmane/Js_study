8a23b2bb521d619681fad27497a7f9a1287242b3
Darshan Mane <126061030+darshanmane@users.noreply.github.com>
2024-02-05 07:29:03 +0000

    datatype

---
+++ .devcontainer/01_basic/02_Datatype.js
@@ -0,0 +1,18 @@
+"use strict"; // treat all JS code as newer version
+
+// alert("hello") // we are using node.js not browser
+
+console.log(3 + 3)   // code readability should be high
+console.log("darshan")
+
+let name = "darshan"
+let age = 31
+let isloggedIn = false
+
+// number --> 2 to power 53
+// bigint 
+// string --> ""
+// boolean --> true/false
+// null --> standalone value
+// undefined --> variable delared but value is not assigned 
+// symbol 
