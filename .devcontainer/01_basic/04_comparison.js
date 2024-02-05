867b9d2bf4f3f0ec961a918ac0b92b2fcd0e48b2
Darshan Mane <126061030+darshanmane@users.noreply.github.com>
2024-02-05 09:43:11 +0000

    comparison

---
+++ .devcontainer/01_basic/04_comparison.js
@@ -0,0 +1,18 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+// console.log(null > 0); //fasle
+// console.log(null == 0); // fasle
+// console.log(null >= 0); // true --> convert the null to a number, treating its 0 
+
+// console.log(undefined > 0); //fasle
+// console.log(undefined == 0);//fasle
+// console.log(undefined < 0);// fasle
+
+// ===
+console.log("2"=== 2);
