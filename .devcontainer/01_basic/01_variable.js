c12082b32ed96c95eaee2fcb3a5770d3db853248
Darshan Mane <126061030+darshanmane@users.noreply.github.com>
2024-02-05 07:13:43 +0000

    variable

---
+++ .devcontainer/01_basic/01_variable.js
@@ -0,0 +1,16 @@
+const accountId = 12345
+let accountEmail = "darshan.mane@gmail.com"
+var accountPassword="147"
+accountcity = "Mumbai"
+
+//accountId = 2
+accountEmail = "sd@gmail.com"
+accountcity = "Jaipur"
+accountPassword = "720"
+//console.log(accountEmail); // not allowed
+
+/*
+do not use var because issue in the block scope and function scope
+*/
+
+console.table([accountId,accountEmail,accountPassword,accountcity])
