946a8207bdb421feaa16e51c291812734f685dce
Darshan Mane <126061030+darshanmane@users.noreply.github.com>
2024-02-06 05:56:35 +0000

    array

---
+++ .devcontainer/02_basic/01_array.js
@@ -0,0 +1,34 @@
+//array
+const myarr = [0, 1, 2, 3, 4, 5]
+const myHeros = ["Shaktiman", "Naagraj"]
+const myarr2 = new Array(1, 2, 3, 4)
+// console.log(myarr[1]);
+
+// Array Meythod
+
+// myarr.push(6)
+// myarr.push(7)
+// myarr.pop()
+
+// myarr.unshift(9)
+// myarr.shift()
+
+// console.log(myarr.includes(9));
+// console.log(myarr.indexOf(5));
+
+const newArr = myarr.join()
+// console.log(myarr);
+// console.log(newArr);
+// console.log(typeof newArr);
+
+// Slice, splice
+// console.log("Org",myarr)
+// const mynewarr1 = myarr.slice(1,3)
+// console.log(mynewarr1)
+// console.log("after slice",myarr)
+
+// console.log("Org",myarr)
+// const mynewarr2 = myarr.splice(1,3)
+// console.log(mynewarr2)
+// console.log("after splice",myarr)
+
