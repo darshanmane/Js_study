2471f89e9d76b1c2f20d6088db8ff5f6a730b429
Darshan Mane <126061030+darshanmane@users.noreply.github.com>
2024-02-07 05:29:30 +0000

    switch

---
+++ .devcontainer/04_control_flow/02_switch.js
@@ -0,0 +1,49 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = 10
+switch (month) {
+    case 1:
+        console.log("January");
+        break;
+    case 2:
+        console.log("feb");
+        break;
+    case 3:
+        console.log("Mar");
+        break;
+    case 4:
+        console.log("Apr");
+        break;
+    case 5:
+        console.log("May");
+        break;
+    case 6:
+        console.log("June");
+        break;
+    case 7:
+        console.log("July");
+        break;
+    case 8:
+        console.log("Aug");
+     case 9:
+         console.log("sep");
+         break;
+        
+    case 10:
+        console.log("OCT");
+        break;
+    case 11:
+        console.log("nov");
+        break;
+
+    default:
+        console.log("dec");
+        break;
+}
