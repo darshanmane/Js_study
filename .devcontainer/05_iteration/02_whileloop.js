ddc9b97ee848bd4d40daf169fec4753051b32cd3
Darshan Mane <126061030+darshanmane@users.noreply.github.com>
2024-02-07 06:33:47 +0000

    whileloop

---
+++ .devcontainer/05_iteration/02_whileloop.js
@@ -0,0 +1,22 @@
+// while
+// while (condition) {
+    
+// }
+//  let  index = 0
+// while (index <= 10) {
+//     console.log(`value of index is ${index}`);
+//     index= index + 2
+// }
+
+// let myarray = ["flash", "batman", "superman"]
+// let index = 0
+// while (index < myarray.length) {
+//     console.log(`value is array is ${myarray[index]}`)
+//     index = index + 1
+// }
+
+let score = 11
+do {
+    console.log(`value of score is : ${score}`);
+    score++
+} while (score <= 10);
