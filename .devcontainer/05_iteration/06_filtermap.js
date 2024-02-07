7bd0ab54e88bcb29fe2c4f6ba9b4bb0d96182d8c
Darshan Mane <126061030+darshanmane@users.noreply.github.com>
2024-02-07 09:17:11 +0000

    filter

---
+++ .devcontainer/05_iteration/06_filtermap.js
@@ -0,0 +1,23 @@
+const coding = ["js", "cpp", "py", "java", "rb"]
+// const newnum  = coding.filter( (item) => item == "java" )
+//  const value = coding.forEach( (item)=> {
+//     // console.log(item);
+//     return item
+// } )
+
+// console.log(value);
+
+const mynum = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+//  const newnum = mynum.filter( (num) => num > 4 )
+// const newnum = mynum.filter( (num) => { return num > 4} )  // in scope we need to use return keyword
+//  console.log(newnum);
+//  const newnums = []
+
+//  mynum.forEach( (num) => {
+//     if (num > 4) {
+//         newnums.push(num)
+//     }
+//  } )
+
+//  console.log(newnums);
