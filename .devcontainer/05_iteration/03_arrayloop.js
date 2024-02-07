0d74a5f234317f28857a8c18051b4ff130b9e286
Darshan Mane <126061030+darshanmane@users.noreply.github.com>
2024-02-07 06:55:53 +0000

    arrayloop

---
+++ .devcontainer/05_iteration/03_arrayloop.js
@@ -0,0 +1,38 @@
+// for of
+// ["", "", ""]
+// [{}, {}, {}]
+
+// const arr = [1, 2, 3, 4, 5]
+// for (const num of arr) {
+//     console.log(num);
+// }
+
+// const greeting = "Hello world !"
+// for (const greet of greeting) {
+//     console.log(`each character is ${greet}`);
+// }
+
+//Maps
+
+const map = new Map()
+map.set('IN','india')
+map.set('USA','United States of American')
+map.set('FI','France')
+map.set('IN','india')
+
+// console.log(map)
+
+// for (const [key,value] of map) {
+//     console.log(key, ' :- ', value);
+    
+// }
+
+const myobj = {
+    'game1' : "NFS",
+    'game2' : "spiderman"
+}
+
+// for (const [key,value] of myobj) {
+//     console.log(key, ' :- ', value);  // object is not iterable
+// }
+
