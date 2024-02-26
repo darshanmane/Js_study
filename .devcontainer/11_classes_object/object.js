34863239fae4618158b59505eb5d3343066d0b8b
Darshan Mane <126061030+darshanmane@users.noreply.github.com>
2024-02-26 10:14:54 +0000

    object

--- .devcontainer/11_classes_object/object.js
+++ .devcontainer/11_classes_object/object.js
@@ -6,3 +6,23 @@ multiple5.power = 2
 console.log(multiple5(5));
 console.log(multiple5.power);
 console.log(multiple5.prototype);
+
+function CreateUser(username, score) {
+    this.username = username,
+    this.score = score
+    
+}
+
+CreateUser.prototype.increment = function () {
+    this.score++
+}
+CreateUser.prototype.printme = function () {
+    console.log(`score is ${this.score}`);
+}
+
+const suresh = new CreateUser("suresh",25)
+const mahesh = new CreateUser("Mahesh",250)
+
+suresh.printme()
+  mahesh.increment()
+  mahesh.printme()
